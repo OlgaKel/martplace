c76da1f6a6670150556c5da43a7ad06a2d3af2e1
OlgaKel <nikkelos@ya.ru>
2023-12-14 09:14:13 +0300

    added index page

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
 
-
-
+     $('.project__items').slick({
+        infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: true,
+      });
+      $('.review__slider').slick({
+            infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: true,
+          });
 
     
 });
+
